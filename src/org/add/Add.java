a2d5a7ac7cc2fb6659903f9861f07fce94762b7b
Vinithkumar1997 <vinithsivakumar14@gmail.com>
2021-01-20 14:19:11 +0530

    Updated A

--- src/org/add/Add.java
+++ src/org/add/Add.java
@@ -3,15 +3,12 @@ package org.add;
 public class Add {
 	public static void main(String[] args) {
 		int a = 10, b = 20;
-<<<<<<< HEAD
-		int c = a + b;
-		System.out.println(c);
-		System.out.println("A Branch");
-=======
 		int d = a + b;
 		System.out.println(d);
 		String name = "Vinith";
 		System.out.println(name);
->>>>>>> 6b690aa439ee37d2c0d41f16b86604a2388b0042
+		int c = a + b;
+		System.out.println(c);
+		System.out.println("A Branch");
 	}
 }
